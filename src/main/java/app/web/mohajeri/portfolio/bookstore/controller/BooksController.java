75caa0370a1e5abcb22efa6fe6b8c80c99bdc888
Masoud Mohajeri <masoudmohajeri.wd@gmail.com>
2022-09-29 18:58:45 +0330

    put and delete method for books

--- src/main/java/app/web/mohajeri/portfolio/bookstore/controller/BooksController.java
+++ src/main/java/app/web/mohajeri/portfolio/bookstore/controller/BooksController.java
@@ -6,11 +6,13 @@ import app.web.mohajeri.portfolio.bookstore.payload.response.GenericResponse;
 import app.web.mohajeri.portfolio.bookstore.repository.BookRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -25,55 +27,70 @@ public class BooksController {
     }
 
     @GetMapping("/all")
-    public String getAllBooks() {
-
+    public ResponseEntity<List<Book>> getAllBooks() {
         List<Book> allBooks = bookRepository.findAll();
-        var res = new GenericResponse<List<Book>>();
-        res.setResponseCode(200);
-        res.setPayload(allBooks);
-        return res.toString();
-
+        return ResponseEntity.status(200).body(allBooks);
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteBook(@PathVariable String id) {
+        Optional<Book> result = bookRepository.findById(id);
+        if (result.isPresent()) {
+            bookRepository.deleteById(id);
+            return ResponseEntity.status(200).body("Book deleted successfully");
+        } else {
+            return ResponseEntity.status(404).body("Book not found ");
+
+        }
+    }
+
+
     @PostMapping("")
-    public String createBook(@Valid @RequestBody Book book, Errors errors) throws Exception {
+    public ResponseEntity<String> createBook(@Valid @RequestBody Book book, Errors errors) throws Exception {
         try {
             if (errors.hasErrors()) {
-                log.info("req payload : " + book.toString());
-                var res = new GenericResponse<String>();
-                res.setResponseCode(400);
-                res.setPayload(errors.getFieldError().toString());
-                return res.toString();
+                return ResponseEntity.status(400).body(errors.getFieldError().toString());
             }
             var findWithIsbn = bookRepository.findBookByIsbn(book.getIsbn());
             log.info("=== findWithIsbn" + findWithIsbn.toString());
             if (bookRepository.findBookByIsbn(book.getIsbn()).isPresent()) {
-                var res = new GenericResponse<String>();
-                res.setResponseCode(400);
-                res.setPayload("Book with ISBN : " + book.getIsbn() + "already exist .");
-                return res.toString();
+                return ResponseEntity.status(400).body("Book with ISBN : " + book.getIsbn() + "already exist .");
             }
 
             var result = bookRepository.save(book);
             if (result != null) {
-                var res = new GenericResponse<Book>();
-                res.setPayload(result);
-                res.setResponseCode(200);
-                return res.toString();
+                return ResponseEntity.status(200).body(result.toString());
             } else {
-                var res = new GenericResponse<String>();
-                res.setPayload("Book not saved : DB issue");
-                res.setResponseCode(500);
-                return res.toString();
+                return ResponseEntity.status(500).body("Book not saved : DB issue");
             }
 
         } catch (Exception e) {
-            var res = new GenericResponse<String>();
-            res.setPayload("Book not saved : Server issue" + e.toString());
-            res.setResponseCode(500);
-            return res.toString();
+            return ResponseEntity.status(200).body("Book not saved : Server issue" + e.toString());
         }
-
     }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> createBook(@Valid @RequestBody Book book, @PathVariable String id, Errors errors) throws Exception {
+        try {
+            if (errors.hasErrors()) {
+                return ResponseEntity.status(400).body(errors.getFieldError().toString());
+            }
+            var result = bookRepository.findById(id);
+            if (!result.isPresent()) {
+                return ResponseEntity.status(200).body("Book with this id doesn't exist ");
+            } else {
+                result.get().setAuthor(book.getAuthor());
+                result.get().setName(book.getName());
+                result.get().setIsbn(book.getIsbn());
+                bookRepository.save(result.get());
+                return ResponseEntity.status(200).body("Book updated successfully");
+            }
+
+        } catch (Exception e) {
+            return ResponseEntity.status(200).body("Book not saved : Server issue" + e.toString());
+        }
+    }
+
 
 }
